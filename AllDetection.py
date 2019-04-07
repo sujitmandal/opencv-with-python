74c904921b6fe0736ba41683aad6c8bcfc94a4ab
sujit mandal <49348037+sujitmandal@users.noreply.github.com>
2019-04-07 10:00:49 +0530

    Update AllDetection.py

--- AllDetection.py
+++ AllDetection.py
@@ -4,7 +4,7 @@ import pandas
 import os
 from datetime import datetime
 
-#This is create bt SUjit Mandal
+#This is create bt Sujit Mandal
 
 first_frame = None
 face = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
